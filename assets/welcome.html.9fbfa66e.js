937f94ef36d9691c42e46baffb28432f9178c5f3
audrey62roberts <audrey.roberts.62@mail.ru>
2022-04-06 15:12:27 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.9fbfa66e.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to Wrap.Pub",lang:"en-US",frontmatter:{index:1,title:"Welcome to Wrap.Pub",author:"Wrap.Pub",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`Wrap Pub : Mortgage assets Wrap Pub.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://wrap.pub/welcome.html"}],["meta",{property:"og:title",content:"Welcome to Wrap.Pub"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"Wrap.Pub"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>Wrap Pub : Mortgage assets Wrap Pub.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"Wrap Pub",slug:"wrap-pub",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why WrapPub?",slug:"why-wrappub",children:[]},{level:2,title:"Tech Stack to Build Wrap Pub",slug:"tech-stack-to-build-wrap-pub",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649229114e3,updatedTime:1649229114e3,contributors:[{name:"audrey62roberts",email:"audrey.roberts.62@mail.ru",commits:1}]},readingTime:{minutes:.45,words:134},filePathRelative:"welcome.md"};export{e as data};
